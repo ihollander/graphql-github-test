5bfe3ee809911795d43a4306e5d74fd491718b3f
Ian Hollander <ianholla@gmail.com>
2019-06-28 10:58:45 -0400

    alternate dynamic page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,59 @@
+import axios from "axios"
+
 exports.createPages = async function ({ actions: { createPage }, graphql }) {
+  // mock fetch to our future CMS backend
   const users = require("./data/users.json")
-  users.forEach(user => {
-    createPage({
-      path: `/${user.github}`,
-      component: require.resolve(`./src/templates/portfolio.js`),
-      context: { github: user.github },
+
+  // i think it expects all pages to be created before completing this function
+  // so Promise.all is necessary for creating all pages
+  await Promise.all(
+    users.map(async user => {
+      /* TODO:
+        - add PORTFOLIO.md file to projects that should be included
+        - filter for only projects that have a PORTFOLIO.md file
+        - get content from PORTFOLIO.md?
+      */
+      const { data } = await graphql(`
+          query($login: String!) {
+            github {
+              user(login: $login) {
+                name
+                bio
+                company
+                email
+                url
+                status {
+                  message
+                }
+                isHireable
+                pinnedItems(first: 6) {
+                  nodes {
+                    ... on GitHub_Repository {
+                      name
+                      homepageUrl
+                      primaryLanguage {
+                        name
+                      }
+                      shortDescriptionHTML
+                      url
+                      object(expression: "master:README.md") {
+                        ... on GitHub_Blob {
+                          text
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `, { "login": user.github })
+
+      return await createPage({
+        path: `/${user.github}`,
+        component: require.resolve(`./src/templates/portfolio.js`),
+        context: { user: data.github.user },
+      })
     })
-  })
+  )
 }
--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -1,48 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
 
 import Portfolio from '../components/Portfolio'
 
-const Index = ({ data }) => {
+export default ({ pageContext: { user } }) => {
+  console.log(user)
   return (
-    <Portfolio user={data.github.user} />
+    <Portfolio user={user} />
   )
 }
-
-export default Index
-
-export const query = graphql`
-  query($github: String!) {
-    github {
-      user(login: $github) {
-        name
-        bio
-        company
-        email
-        url
-        status {
-          message
-        }
-        isHireable
-        pinnedItems(first: 6) {
-          nodes {
-            ... on GitHub_Repository {
-              name
-              homepageUrl
-              primaryLanguage {
-                name
-              }
-              shortDescriptionHTML
-              url
-              object(expression: "master:README.md") {
-                ... on GitHub_Blob {
-                  text
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
